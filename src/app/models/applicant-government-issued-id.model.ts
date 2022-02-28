c1b08ba03cffbdedf34faafd424f947304a12727
Oliver Enciso <oliverenciso07@gmail.com>
2022-02-28 10:56:09 +0800

    remove unrelated files

--- src/app/models/applicant-government-issued-id.model.ts
+++
@@ -1,8 +0,0 @@
-export class ApplicantGovernmentIssuedIdModel {
-  id: number = 0;
-  applicant_id: number = 0;
-  id_name: string = '';
-  id_number: string = '';
-  date_issuance: any;
-  place_issuance: string = '';
-}
